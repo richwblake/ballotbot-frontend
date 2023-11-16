204dba65d2de8993a8fae8381f4bd08b98791700
Wills Blake <richwblake@gmail.com>
2023-11-16 16:03:06 -0500

    Changes welcome message

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -2,8 +2,8 @@ export default function HelloMessage() {
     return (
         <div id="intro-message">
             <h2>Welcome to BallotBot</h2> 
-            <p>Please click the button above to create a new ballot!</p>
-            <p>Once you've created your ballot, copy the link in the address bar and send to friends or colleagues to get responses</p>
+            <p>Please click the button below to create a new ballot!</p>
+            <p>Once you've created your ballot, click the Copy Ballot URL button and send to friends or colleagues to get responses</p>
         </div>
     );
 }
