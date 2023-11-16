06cc5a2241805b82ada8eacff15c8837dd46c4b2
Wills Blake <richwblake@gmail.com>
2023-11-16 15:16:14 -0500

    Removes network call and state variables associated from Ballot

--- src/components/ballot.jsx
+++ src/components/ballot.jsx
@@ -1,14 +1,11 @@
 import { useState, useRef, useEffect } from 'react';
 import ClipboardButton from './clipboardButton';
-import { fetchBallotById, formatTimeLeft } from '../utils';
+import { formatTimeLeft } from '../utils';
 
-export default function Ballot({ id }) {
+export default function Ballot({ ballot }) {
 
-    const [ballot, setBallot] = useState({ pubId: null, title: "", seconds_since_creation: null, exp_s: null, responses: [] });
     const [secondsLeft, setSecondsLeft] = useState(ballot.exp_s - ballot.seconds_since_creation);
-    const [timeRunning, setTimeRunning] = useState(false);
     let intervalRef = useRef();
-
 
     // const debug = {
     //     ballot_created: new Date(ballot.created_utc),
@@ -19,22 +16,10 @@ export default function Ballot({ id }) {
     // console.log("Time since creation in seconds: " + (debug.now - debug.ballot_created) / 1000);
 
     useEffect(() => {
-        async function fetchData() {
-            setBallot(await fetchBallotById(id));
-        }
-        fetchData();
+        intervalRef.current = setInterval(() => setSecondsLeft(prev => prev - 1), 1000);
 
         return () => clearInterval(intervalRef.current);
     }, []);
-
-    useEffect(() => {
-        setSecondsLeft(ballot.exp_s - ballot.seconds_since_creation);
-        if (!timeRunning) {
-            intervalRef.current = setInterval(() => setSecondsLeft(prev => prev - 1), 1000);
-            setTimeRunning(true)
-        }
-    }, [ballot]);
-
 
     const renderResponses = responses => {
         return responses.map(r => (
