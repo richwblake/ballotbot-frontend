3efd778bf2befe96ca0f14c567d153ff87850cdb
Wills Blake <richwblake@gmail.com>
2023-11-18 16:08:29 -0500

    Adds functionality to Ballot that detects and marks winning response or responses if a tie occurs

--- src/components/ballot.jsx
+++ src/components/ballot.jsx
@@ -5,7 +5,33 @@ import { calculateExpiry, formatTimeLeft } from '../utils';
 export default function Ballot({ ballot }) {
 
     const [secondsLeft, setSecondsLeft] = useState(calculateExpiry(ballot));
+    const [winners, setWinners] = useState([]);
     let intervalRef = useRef();
+
+    const findAndSetWinnerIds = () => {
+        let maxVotes = ballot.responses[0].votes;
+        let currentWinners = [];
+        ballot.responses.forEach(r => {
+            if (r.votes > maxVotes) {
+                currentWinners = [r];
+                maxVotes = r.votes;
+            } else if (r.votes === maxVotes) {
+                currentWinners.push(r);
+            }
+        });
+        setWinners(currentWinners);
+    };
+
+    const renderResponses = () => {
+        const winMsg = winners.length === 1 ? "(Winner!)" : "(Tie!)";
+        return ballot.responses.map(r => {
+            const respIsWinner = winners.find(w => w.pubId === r.pubId);
+
+            return (<div className={`responses ${respIsWinner ? "winner" : ""}`} key={r.pubId}>
+                <h2 className='response-title'>{r.content}</h2>
+                <p>Votes: {r.votes + " " + (respIsWinner ? winMsg : "")}</p>
+            </div>)});
+    }
 
     useEffect(() => {
         intervalRef.current = setInterval(() => setSecondsLeft(prev => prev - 1), 1000);
@@ -16,17 +42,10 @@ export default function Ballot({ ballot }) {
     useEffect(() => {
         if (secondsLeft <= 0) {
             setSecondsLeft(0);
+            findAndSetWinnerIds();
             clearInterval(intervalRef.current);
         }
     }, [secondsLeft]);
-
-    const renderResponses = () => {
-        return ballot.responses.map(r => (
-            <div className='responses' key={r.pubId}>
-                <h2 className='response-title'>{r.content}</h2>
-                <p>Votes: {r.votes}</p>
-            </div>));
-    }
 
     return (
         <div id='ballot'>
